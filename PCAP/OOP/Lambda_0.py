90303b5323422f60c11182021daaca70171e8808
Alvaro Avila <alu-alvavimar@foc.es>
2025-03-10 16:51:21 +0100

    ejercicios python post-examenes

---
+++ PCAP/OOP/Lambda_0.py
@@ -0,0 +1,16 @@
+''' Funciones lambda (0) '''
+
+# (1) Función lambda que calcula el área de un triángulo
+area_triangulo = lambda base, altura: (base * altura) / 2
+
+# (2) Función lambda que calcula el cubo de un número entero
+al_cubo = lambda num: num ** 3
+
+# (3) Función lambda que modifica la salida de una cadena (string)
+destacar_valor = lambda valor: f"¡¡¡{valor}!!!"
+
+comision_empleado_1 = 15500
+
+print(destacar_valor(comision_empleado_1))
+print("Alvaro Avila Martinez")
+
